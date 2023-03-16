2cdad1ed30ed126f935ece417a4e26f89e64d5cb
Daryll Ko <daryll.ko@gmail.com>
2023-03-16 21:13:41 +0800

    feat: setup basic structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>SN P Results</title>
+        <meta
+          name="description"
+          content="Collection of results on SN P systems"
+        />
+        <link rel="icon" href="/favicon.svg" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <div className="font-display min-h-screen dark:bg-black dark:text-white">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
