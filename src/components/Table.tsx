caf360a73ec1ec4aa9cc35fd53a7891dd9ef73a4
Daryll Ko <daryll.ko@gmail.com>
2023-03-19 11:59:31 +0800

    refactor: extract table data from markup

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,21 +1,33 @@
 const Table = () => {
+  const columns = ["Name", "Acronym"];
+
+  const data = [
+    {
+      name: "Standard",
+      acronym: "SN P",
+    },
+    {
+      name: "Structural Plasticity",
+      acronym: "SNPSP",
+    },
+  ];
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Acronym</th>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Standard</td>
-          <td>SN P</td>
-        </tr>
-        <tr>
-          <td>Structural Plasticity</td>
-          <td>SNPSP</td>
-        </tr>
+        {data.map(({ name, acronym }, index) => (
+          <tr key={index}>
+            <td>{name}</td>
+            <td>{acronym}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
