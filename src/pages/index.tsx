c74bb8d065271b2bd5188f65d1af3510211b96d7
Daryll Ko <daryll.ko@gmail.com>
2023-03-19 12:11:00 +0800

    fix: add space to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Table from "~/components/Table";
 const Home: NextPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-5xl">SNP Results</h1>
+      <h1 className="text-5xl">SN P Results</h1>
       <Table />
     </main>
   );
