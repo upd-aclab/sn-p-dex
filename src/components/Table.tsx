0a8d5b41832e802b2afd3dd92531583f4adfe1e7
Daryll Ko <daryll.ko@gmail.com>
2023-03-19 11:20:05 +0800

    feat: add initial table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,24 @@
 const Table = () => {
-  return <div>Table</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Acronym</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Standard</td>
+          <td>SN P</td>
+        </tr>
+        <tr>
+          <td>Structural Plasticity</td>
+          <td>SNPSP</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
